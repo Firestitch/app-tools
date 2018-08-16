238aaaf0dd37e70be1279d3448767e31721ddc06
Raymond <ray@firestitch.com>
2018-08-16 09:28:34 -0400

    grabbed only the arguments from the json project.name

--- scripts/build-env.js
+++ scripts/build-env.js
@@ -3,22 +3,28 @@
 const fs = require('fs');
 const path = require('path');
 
-var data = {};
-Object.keys(process.env).forEach((name) => {
+const envFile = path.join(__dirname, '../../src/environments/env.ts');
+const jsonFile = path.join(__dirname, '../../package.json');
+const pacakgeJson = JSON.parse(fs.readFileSync(jsonFile));
+const packageName = pacakgeJson.name;
 
-	var match = name.match(/npm_package_config_(.*)/);
+var data = {};
+var re = new RegExp('npm_config_' + packageName + '_(.*)');
+
+Object.keys(process.env).forEach((name) => {
+npm_config_sportgo_device:
+	var match = name.match(re);
 	if (match) {
 		var value = process.env[name];
 		data[match[1]] = value;
 	}
 });
 
-
-const envFile = path.join(__dirname, '../../src/environments/env.ts');
-
 data = JSON.stringify(data);
 
 var output = 'export const env = ' + data + ';';
+
+console.log('\x1b[36m%s\x1b[0m','Env Variables (env.ts) ',data);
 
 fs.writeFileSync(envFile, output);
 
